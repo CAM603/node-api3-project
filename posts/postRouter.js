010aa640f08c7ea3ed1eaa2f57c56398a64ae927
CAM603 <cameronhawley@comcast.net>
2020-02-12 19:20:59 -0500

    ADD get post by id

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,49 @@
 const express = require('express');
 
+const Posts = require('./postDb');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
+
+  Posts.get()
+    .then(posts => {
+      res.status(500).json(posts)
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Error getting posts'})
+    })
+});
+
+router.get('/:id', validatePostId, (req, res) => {
+  res.json(req.post)
+});
+
+router.delete('/:id', validatePostId, (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put('/:id', validatePostId, (req, res) => {
   // do your magic!
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  const { id } = req.params;
+
+  Posts.getById(id)
+    .then(post => {
+      if(post) {
+        req.post = post;
+        next()
+      } else {
+        res.status(400).json({ message: "invalid post id" })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "error getting post" })
+    })
 }
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const usersRouter = require('./users/userRouter');
+const postsRouter = require('./posts/postRouter');
 
 const server = express();
 
@@ -9,6 +10,7 @@ server.use(express.json());
 
 // routes - endpoints
 server.use('/api/users', logger, usersRouter);
+server.use('/api/posts', logger, postsRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
